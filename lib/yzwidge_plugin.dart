4591a2974ad33840c8608f906636a89f7af0782b
chiyz <3004346919@qq.com>
2019-06-17 11:45:17 +0800

    feat：搭建新的package

---
+++ lib/yzwidge_plugin.dart
@@ -0,0 +1,9 @@
+library yzwidge_plugin;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  int addTwoNumber(int value1, int value2) => value1 + value2;
+}
