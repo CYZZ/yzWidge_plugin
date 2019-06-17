ef5545045e16662d80754c06fc04008213545884
chiyz <3004346919@qq.com>
2019-06-17 11:47:59 +0800

    feat：添加乘法方法

--- lib/yzwidge_plugin.dart
+++ lib/yzwidge_plugin.dart
@@ -6,4 +6,6 @@ class Calculator {
   int addOne(int value) => value + 1;
 
   int addTwoNumber(int value1, int value2) => value1 + value2;
+
+  double multiplyTwoNumber(double value1, double value2) => value1 * value2;
 }
